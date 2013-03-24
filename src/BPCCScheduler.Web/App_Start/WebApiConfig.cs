f7b31e75c9092b8adaa12d36afd3ab4225c43d86
dkitchen <github@dkitchen.net>
2013-03-24 13:25:30 -0400

    elmah error handling for webapi

--- src/BPCCScheduler.Web/App_Start/WebApiConfig.cs
+++ src/BPCCScheduler.Web/App_Start/WebApiConfig.cs
@@ -9,6 +9,11 @@ namespace BPCCScheduler
     {
         public static void Register(HttpConfiguration config)
         {
+            //DMK 2013-03-24 added elmah error handling for webapi
+            config.Filters.Add(
+                new ElmahErrorAttribute()
+            );
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
---
+++ src/BPCCScheduler.Web/ElmahErrorAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BPCCScheduler
+{
+    public class ElmahErrorAttribute :
+        System.Web.Http.Filters.ExceptionFilterAttribute
+    {
+
+        public override void OnException(
+            System.Web.Http.Filters.HttpActionExecutedContext actionExecutedContext)
+        {
+
+            if (actionExecutedContext.Exception != null)
+            {
+                Elmah.ErrorSignal.FromCurrentContext().Raise(actionExecutedContext.Exception);
+            }
+
+            base.OnException(actionExecutedContext);
+
+        }
+    }
+
+}
