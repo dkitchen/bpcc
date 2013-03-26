ab6a3d0aaac7366105c3d3377d108053ea1bb790
Denis M. Kitchen <github@dkitchen.net>
2013-03-26 11:26:05 -0300

    Update SMSTodayAMController.cs

--- src/BPCCScheduler.Web/Controllers/SMSTodayAMController.cs
+++ src/BPCCScheduler.Web/Controllers/SMSTodayAMController.cs
@@ -17,8 +17,11 @@ namespace BPCCScheduler.Controllers
             //any appointment today after last-night midnight, but before today noon
             var lastNightMidnight = DateTime.Now.Date;
             var ret = "";
-            ret += lastNightMidnight.ToLongDateString();            
+            ret += lastNightMidnight.ToLongDateString();
+            ret += " " + lastNightMidnight.ToLongTimeString()
             var todayNoon = lastNightMidnight.AddHours(12);
+            ret += " " + todayNoon.ToLongDateString();
+            ret += " " + todayNoon.ToLongTimeString()
             return ret;
             var appts = base.AppointmentContext.Appointments.ToList()    //materialize for date conversion
                 .Where(i => i.Date.ToLocalTime() > lastNightMidnight && i.Date.ToLocalTime() < todayNoon);    
